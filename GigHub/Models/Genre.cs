23df5f021b77fc37766ed10d4013237f671f1525
michaalmar <michalmarchela@gmail.com>
2019-07-09 12:08:22 +0200

    refactor Gig,genre model

--- GigHub/Models/Genre.cs
+++ GigHub/Models/Genre.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.Models
 {
     public class Genre
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        
     }
 }
--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.Models
 {
@@ -7,9 +8,17 @@ namespace GigHub.Models
 
 
         public int Id { get; set; }
+
+        [Required]
         public ApplicationUser Artist { get; set; }
+
         public DateTime DateTime { get; set; }
+        [Required]
+
+        [StringLength(255)]
         public string Venue { get; set; }
+
+        [Required]
         public Genre Genre { get; set; }
 
 
